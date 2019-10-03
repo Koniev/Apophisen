172ca73df6dcb773a2d3737a353695b4e5cc0d99
Koniev <gautier.carree@isen-lille.fr>
2019-10-03 03:21:33 +0200

    add new key, deal with matchDetail

--- GetGameDetail.py
+++ GetGameDetail.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Sep 27 21:15:16 2019
+Created on Wed Oct  2 22:21:55 2019
 
 @author: gauti
 """
+
 
 from riotwatcher import RiotWatcher, ApiError
 import json
@@ -11,16 +12,12 @@ import json
 
 watcher = RiotWatcher('RGAPI-e64d043b-400e-42a2-8a3a-07e387e18ac9')
 my_region = 'euw1'
-my_name = 'Koniev'
-my_queue = 420 # 'RANKED_SOLO_5x5'
 
 
-def getPlayerData(region, summonerName, queueNumber):
+def getGameData(region, gameId):
     try:
-        # Get summoner Id
-        player = watcher.summoner.by_name(region, summonerName)
-        # Then get history based on this Id
-        history = watcher.match.matchlist_by_account(region, player.get('accountId'), queueNumber)
+        # Get game details
+        game = watcher.match.by_id(region, gameId)
         # TO DO : matchDetails to deal with correctly
         # matchsDetails = [watcher.match.by_id(region, match.get('gameId')) for match in history.get('matches')]
         with open('9.19.1/data/fr_FR/champion.json', "r") as champData:
@@ -31,15 +28,11 @@ def getPlayerData(region, summonerName, queueNumber):
             key = champInfos.get(c).get('key')
             if key not in champs.keys():
                 champs[key] = champInfos.get(c).get('name')
-        # TO DELETE : dump champion references just to check if it is working correctly
-        with open("champRef.json", "w") as r:
-             r.write(json.dumps(champs))
         # Store data in data-SummonerName.json
-        with open("data-" + summonerName + ".json","w") as f:
-            matches = history.get('matches')
-            for item in matches:
-                item['champion']=champs.get(str(item['champion'])) if champs.get(str(item['champion'])) != None else item['champion']
-            f.write(json.dumps(matches))
+        with open("gameData-" + gameId + ".json","w") as f:
+            for item in game['participants']:
+                item['championId'] = champs.get(str(item['championId'])) if champs.get(str(item['championId'])) != None else item['championId']
+            f.write(json.dumps(game))
     except ApiError as err:
         if err.response.status_code == 429:
             print('We should retry in {} seconds.'.format(err.response.headers['Retry-After']))
@@ -50,7 +43,4 @@ def getPlayerData(region, summonerName, queueNumber):
         else:
             raise
             
-getPlayerData(my_region, 'Koniev', my_queue)
-getPlayerData(my_region, 'APO Clown', my_queue)
-getPlayerData(my_region, 'Neekoptère', my_queue)
-getPlayerData(my_region, 'PieraptorR', my_queue)
+getGameData(my_region, '4211509827')
