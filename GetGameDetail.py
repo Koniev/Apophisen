2bf9845c67d7e651c79786faa63a9dd3486a3552
carreeg <gautier.carree@ifsttar.fr>
2019-10-09 13:55:03 +0200

    Get specific data and create object player which stored the data we need

--- GetGameDetail.py
+++ GetGameDetail.py
@@ -10,16 +10,38 @@ from riotwatcher import RiotWatcher, ApiError
 import json
         
 
-watcher = RiotWatcher('RGAPI-e64d043b-400e-42a2-8a3a-07e387e18ac9')
+watcher = RiotWatcher('RGAPI-22522138-52b3-419f-84ac-bc8e1146c813')
 my_region = 'euw1'
 
 
+class Player:
+    Lane = ""
+    Champion = ""
+    Kills = 0
+    Deaths = 0
+    Assists = 0
+    Damages = 0
+    def __init__(self, name, team, champion=None, lane=None,  kills=None, deaths=None, assists=None, damages=None):
+        self.name = name
+        self.Team = team
+        self.Lane = lane
+        self.Champion = champion
+        self.Kills = kills
+        self.Deaths = deaths
+        self.Assists = assists
+        self.Damages = damages
+    def __str__(self):
+        string = ""
+        for name, value in vars(self).items():
+            string += name + ": " + str(value) + "\n"
+        return string
 def getGameData(region, gameId):
     try:
         # Get game details
         game = watcher.match.by_id(region, gameId)
         # TO DO : matchDetails to deal with correctly
         # matchsDetails = [watcher.match.by_id(region, match.get('gameId')) for match in history.get('matches')]
+        listOfPlayers = []
         with open('9.19.1/data/fr_FR/champion.json', "r") as champData:
             champJson = json.load(champData)
         champInfos = champJson.get('data')
@@ -38,7 +60,9 @@ def getGameData(region, gameId):
             for item in game['participants']:
                 item['championId'] = champs.get(str(item['championId'])) if champs.get(str(item['championId'])) != None else item['championId']
                 item['participantId'] = playerDict.get(item['participantId'])
-            f.write(json.dumps(game))
+                stats = item['stats']
+                listOfPlayers.append(Player(item['participantId'], item['teamId'],item['championId'],"BOTTOM", stats['kills'], stats['deaths'], stats['assists'], stats['totalDamageDealtToChampions']))               
+            f.write(json.dumps([player.__dict__ for player in listOfPlayers]))
     except ApiError as err:
         if err.response.status_code == 429:
             print('We should retry in {} seconds.'.format(err.response.headers['Retry-After']))
